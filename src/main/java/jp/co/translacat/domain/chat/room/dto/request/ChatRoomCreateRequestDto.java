c2612b126040792093d24d0f4968410b3a223b7a
kukkoro-admin <bellline0403@gmail.com>
2026-06-21 19:20:46 +0900

    2026-06-21 개발분 반영 - 룸 생성 시, 검증 어노테이션 추가.

--- src/main/java/jp/co/translacat/domain/chat/room/dto/request/ChatRoomCreateRequestDto.java
+++ src/main/java/jp/co/translacat/domain/chat/room/dto/request/ChatRoomCreateRequestDto.java
@@ -1,13 +1,19 @@
 package jp.co.translacat.domain.chat.room.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import jp.co.translacat.domain.chat.room.enums.ChatRoomType;
 
 import java.util.List;
 
 public record ChatRoomCreateRequestDto(
+        @NotNull(message = "채팅방 타입은 필수입니다.")
         ChatRoomType roomType,
+
         String name,
+
         String description,
+
+        @NotNull(message = "채팅방 멤버 목록은 필수입니다.")
         List<Long> memberUserIds
 ) {
 }
